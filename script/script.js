634ed35effeadc7089d791ab9edbf114af94eebd
tehuanmelo <tehuanmelo@gmail.com>
2025-01-24 23:11:06 +0400

    few layout changes

--- script/script.js
+++ script/script.js
@@ -7,7 +7,6 @@ const projects = document.getElementsByClassName('project-detail-container')
 Array.from(projects).forEach(element => {
     element.onclick = () => {
         const projectDetails = element.querySelector('.project-details')
-        console.log(projectDetails.style.display)
         if (projectDetails.style.display === 'none' || projectDetails.style.display === '') {
             projectDetails.style.display = 'block';
         } else {
